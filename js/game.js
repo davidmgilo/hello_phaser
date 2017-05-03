68838ef4d5f16a6b26012c5c85cf66a76bfd3c65
David Martinez <davidmgilo@gmail.com>
2017-05-03 19:32:15 +0200

    imported phaser - starting with a logo

---
+++ js/game.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    // var game = new Phaser('100','100') %
+    // var game = new Phaser(800, 600, Phaser.AUTO, '#game') si es deixa buit, va al body.
+
+    var states = {
+        preload: preload,
+        create: create
+    }
+
+    var game = new Phaser(800, 600, Phaser.AUTO, '', states)
+
+    function preload() {
+        //LoadManager
+        game.load.image('logo','assets/phaser.jpg')
+    }
+    
+    function create() {
+        //World
+        game.add.sprite(0,0,'logo')
+        // game.add.sprite(game.world.centerX,0, )
+    }
+}
